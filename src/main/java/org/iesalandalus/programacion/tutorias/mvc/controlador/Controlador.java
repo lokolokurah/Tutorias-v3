657804b9499acbdaf8df6fb35c267991e13fe957
Jaime <Jaime@HYRULE-5500X>
2020-03-02 19:35:16 +0100

    10. Modificada Clase Controlador.

--- src/main/java/org/iesalandalus/programacion/tutorias/mvc/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/controlador/Controlador.java
@@ -35,12 +35,14 @@ public class Controlador implements IControlador {
 	@Override
 	public void comenzar() 
 	{
+		modelo.comenzar();
 		vista.comenzar();
 	}
 
 	@Override
 	public void terminar() 
 	{
+		modelo.terminar();
 		System.out.println("Fin de la ejecución.");
 	}
 
